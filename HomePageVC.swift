0509d7856a709fed319cdbc3f333d984bb1c495c
ROBIN GEORGE <therobins.dev@gmail.com>
2021-10-28 12:13:41 +0530

    no update on view

--- HomePageVC.swift
+++ HomePageVC.swift
@@ -10,12 +10,12 @@ import UIKit
 import CoreLocation
 
 class HomePageVC: UIViewController, UITableViewDelegate, CLLocationManagerDelegate {
-
-
-
+    
+    
+    
     @IBOutlet weak var weatherTable: UITableView!
     
-    var weather = [Weather]()
+    var weather = [WeatherData]()
     
     let locationManager = CLLocationManager()
     var currentLocation : CLLocation?
@@ -32,17 +32,65 @@ class HomePageVC: UIViewController, UITableViewDelegate, CLLocationManagerDelega
         weatherTable.register(HourlyCollectionViewCell.nib(), forCellReuseIdentifier: "HourlyCollectionViewCell")
         weatherTable.register(WeatherCell.nib(), forCellReuseIdentifier: "WeatherCell ")
         
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        setupLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if !locations.isEmpty, currentLocation == nil{
             currentLocation =  locations.first
             locationManager.stopUpdatingLocation()
+            weatherForLocation()
+            
             
         }
     }
-    //location
     
+    func weatherForLocation(){
+        guard let currentLocation = currentLocation else {
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        
+        print("\(long)| \(lat) ")
+        
+        let url = "api.openweathermap.org/data/2.5/weather?lat=37.785834&lon=-122.406417&appid=7a6f50d61e91ebe14eca463f3afd0895"
+//        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler:
+//         { data, response, error in
+//            guard let data = data , error == nil else {
+//                return
+//            }
+            
+            URLSession.shared.dataTask(with: URL(string: url)!,completionHandler: {data, response, error in
+                //validation
+                guard let data = data, error == nil else {
+
+                    return
+                }
+            
+      
+        var json: WeatherData?
+        
+        do {
+            json = try JSONDecoder().decode(WeatherData.self,from: data)
+        } catch  {
+            print("error")
+        }
+            
+            guard let  result = json else {
+                return
+            }
+            print(result.weather)
+        }).resume()
+}
+    
+    //location
     func setupLocation(){
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
@@ -50,10 +98,7 @@ class HomePageVC: UIViewController, UITableViewDelegate, CLLocationManagerDelega
         
     }
 
- 
 }
-
-
 
 extension HomePageVC : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -67,6 +112,14 @@ extension HomePageVC : UITableViewDataSource{
     
 }
 
-struct Weather {
+struct WeatherData : Codable{
+    var weather : [Details]
+}
+
+struct Details : Codable{
+    var id : Int
+    var main : String
+    var description : String
+    var icon : String
     
 }
